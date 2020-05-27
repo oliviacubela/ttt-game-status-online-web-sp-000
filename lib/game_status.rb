5b10c96adfaee60e06ebf2f841093282ed36cb64
Olivia Cubela <oliviacubela@gmail.com>
2020-05-27 11:53:06 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -31,6 +31,7 @@ def won?(board)
    win_index_1 = win_combination[0]
    win_index_2 = win_combination[1]
    win_index_3 = win_combination[2]
+end
 
   position_1 = board[win_index_1]
   position_2 = board[win_index_2]
