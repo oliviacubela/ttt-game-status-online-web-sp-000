1ebf33ad4535518fd0bf07dbcf7cef3dd907301e
Olivia Cubela <oliviacubela@gmail.com>
2020-05-27 12:33:15 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,18 +3,6 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-win_combination = [
-  ["X", "X", "X", "O", "O", " ", " ", " ", " "],
-  ["O", "O", " ", "X", "X", "X", " ", " ", " "],
-  [" ", " ", " ", "O", "O", " ", "X", "X", "X"],
-  ["O", " ", "X", "O", " ", "X", "O", " ", " "],
-  ["X", " ", "O", " ", "X", "O", " ", " ", "X"],
-  ["X", " ", "O", "X", " ", "O", " ", " ", "O"],
-  ["X", " ", "O", " ", "X", "O", " ", " ", "X"],
-  ["X", "O", "X", "O", "X", "O", "X", "X", "O"]
-]
-
-
 WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
@@ -27,7 +15,7 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
- WIN_COMBINATIONS.each do |win_combination|
+ WIN_COMBINATIONS.each do | win_combination |
    win_index_1 = win_combination[0]
    win_index_2 = win_combination[1]
    win_index_3 = win_combination[2]
