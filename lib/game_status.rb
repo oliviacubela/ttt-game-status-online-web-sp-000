b30ad44995a6e780ebb926162e913a191265856a
Olivia Cubela <oliviacubela@gmail.com>
2020-05-27 09:39:43 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,15 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+WIN_COMBINATIONS = [
+  ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+  ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
+  [" ", " ", "X", " ", "X", " ", "X", " ", " "]
+  [" ", " ", " ", "X", "X", "X", " ", " ", " "]
+  [" ", " ", " ", " ", " ", " ", "X", "X", "X"]
+  [" ", "X", " ", " ", "X", " ", " ", "X", " "]
+  ["X", " ", " ", "X", " ", " ", "X", " ", " "]
+  [" ", " ", "X", " ", " ", "X", " ", " ", "X"]
+  
+]
 # Define your WIN_COMBINATIONS constant
