bd072a7ffd09274240b93b981945f1696c822e26
Olivia Cubela <oliviacubela@gmail.com>
2020-05-27 10:42:15 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,9 +26,5 @@ WIN_COMBINATIONS = [
   [2,5,8]
 ]
 
-def won?(board)
- WIN_COMBINATIONS.each do |win_combination|
-   win_index_1 = win_combination[0]
-   win_index_2 = win_combination[1]
-   win_index_3 = win_combination[2]
+
 # Define your WIN_COMBINATIONS constant
